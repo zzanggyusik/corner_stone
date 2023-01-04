11a44b556bdaeebf3dfe798a74cb7a227482503d
jaehyeok34 <20201753@edu.hanbat.ac>
2023-01-04 21:24:20 +0900

    21:24

--- CornerstoneChatbot.py
+++ CornerstoneChatbot.py
@@ -171,14 +171,3 @@ class CornerstoneChatbot:
     '''
     def fallbackHandler(self, update:Update, context:CallbackContext):
         update.message.reply_text('이용해 주셔서 감사합니다.')
-
-# #============= Call ==============#
-# bot = CornerstoneChatbot()
-# updater = Updater('5936320630:AAGPcpJQfVwN6V5aYMstT1jBkvwn2hhsubI')
-# updater.dispatcher.add_handler(bot.mainHandler)
-# updater.start_polling()
-# updater.idle()
-
-
-
-
