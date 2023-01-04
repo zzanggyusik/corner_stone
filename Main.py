d5beebd1e5d2acbeee65efcd22f442c8211225ec
jaehyeok34 <20201753@edu.hanbat.ac>
2023-01-04 21:23:53 +0900

    21:23

---
+++ Main.py
@@ -0,0 +1,7 @@
+from CornerstoneChatbot import *
+
+bot = CornerstoneChatbot()
+updater = Updater('5936320630:AAGPcpJQfVwN6V5aYMstT1jBkvwn2hhsubI')
+updater.dispatcher.add_handler(bot.mainHandler)
+updater.start_polling()
+updater.idle()
