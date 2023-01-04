91ffdb83370c5479513f0ae0ced3570f6340d6e4
jaehyeok34 <20201753@edu.hanbat.ac>
2023-01-04 17:50:59 +0900

    2023-01-04

--- JH/TelegramChatbot.py
+++ JH/TelegramChatbot.py
@@ -11,19 +11,22 @@ token = '5936320630:AAGPcpJQfVwN6V5aYMstT1jBkvwn2hhsubI'
 class Cornerstone:
     def __init__(self) -> None:
         self.user_id = '0'
-        self.ENTRY_POINT, self.LOCATION_BUTTON, self.LANGUAGE_BUTTON = range(3)
+        self.ENTRY_POINT = 1
+        self.LOCATION_BUTTON = 2
+        self.LANGUAGE_BUTTON = 3
+        self.CANCEL = 4
         self.mainHandler = ConversationHandler(
-                entry_points = [CommandHandler('start', self.location_btn)],
+                entry_points = [CommandHandler('start', self.locationButtonHandler)],
                 states = {
-                    self.LOCATION_BUTTON : [CallbackQueryHandler(self.language_btn)],
+                    self.LOCATION_BUTTON : [CallbackQueryHandler(self.languageButtonHandler)],
                 },
-                fallbacks = [CommandHandler('cancel',self.language_btn)],
-
+                fallbacks = [CommandHandler('cancel',self.fallbackHandler)],
                 map_to_parent={
                     ConversationHandler.END:ConversationHandler.END
                 }
             )
-    def sendMessages(self, update:Update):
+
+    def introduction(self, update:Update):
             update.message.reply_text('안녕하세요, 코너스톤 챗봇입니다.')
             return self.ENTRY_POINT
 
@@ -33,25 +36,24 @@ class Cornerstone:
             menu.insert(0, header_buttons)
         if footer_buttons:
             menu.append(footer_buttons)
-
         return menu
 
-    def location_btn(self, update:Update, context:CallbackContext) -> None:
-        self.sendMessages(update=update)
+    def locationButtonHandler(self, update:Update, context:CallbackContext) -> None:
+        self.introduction(update=update)
         self.user_id = update.effective_chat.id
         btn_list = []
         btn_list.append(InlineKeyboardButton("대전광역시", callback_data="대전광역시"))
         btn_list.append(InlineKeyboardButton("충북", callback_data="충북"))
 
         show_markup = InlineKeyboardMarkup(self.build_menu(btn_list, len(btn_list)))
-        #update.message.reply_text("거주하시는 지역을 선택해 주세요.", reply_markup = show_markup)
         context.bot.send_message(chat_id=self.user_id, 
             text='거주하시는 지역을 선택해 주세요.', 
-            reply_markup=show_markup)
+            reply_markup=show_markup
+            )
 
         return self.LOCATION_BUTTON
 
-    def language_btn(self, update:Update, context:CallbackContext) -> None:
+    def languageButtonHandler(self, update:Update, context:CallbackContext) -> None:
         query = update.callback_query.data
         print(query)
 
@@ -63,9 +65,14 @@ class Cornerstone:
         show_markup = InlineKeyboardMarkup(self.build_menu(btn_list, len(btn_list)))
         context.bot.send_message(chat_id=self.user_id, 
             text='사용하실 언어를 선택해 주세요.', 
-            reply_markup=show_markup)
+            reply_markup=show_markup
+            )
 
         return self.LANGUAGE_BUTTON
+    
+    def fallbackHandler(self, update:Update, context:CallbackContext):
+        update.message.reply_text('이용해 주셔서 감사합니다.')
+        return self.CANCEL
 
 bot = Cornerstone()
 updater = Updater(token)
