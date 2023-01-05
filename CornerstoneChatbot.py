3cf2ff2df983a01c9c51b1636ae2f105996c3dd7
jaehyeok34 <20201753@edu.hanbat.ac>
2023-01-05 14:03:19 +0900

    14:03

--- CornerstoneChatbot.py
+++ CornerstoneChatbot.py
@@ -10,9 +10,9 @@ import prettytable as pt
 class CornerstoneChatbot:
     def __init__(self) -> None:
         #============== User Data ==============#
-        self.user_id = ''           # 사용자의 ID, self.locationButtonHandler에서 값이 저장 됨
-        self.location = ''          # 선택한 지역, self.languageButtonHandler에서 값이 저장 됨
-        self.language = ''          # 선택한 언어, self.sendMessageHandler에서 값이 저장 됨
+        self.user_id = ''           # 사용자의 ID, self.locationHandler에서 값이 저장 됨
+        self.location = ''          # 선택한 지역, self.languageHandler에서 값이 저장 됨
+        self.language = ''          # 선택한 언어, self.messageHandler에서 값이 저장 됨
         #============ Return const =============#
         self.LOCATION_BUTTON = 1    
         self.LANGUAGE_BUTTON = 2
@@ -35,10 +35,10 @@ class CornerstoneChatbot:
         # map_to_parent : 정확히 모르겠음, 없으면 오류 남
         '''
         self.mainHandler = ConversationHandler(
-                entry_points = [CommandHandler('start', self.locationButtonHandler)],
+                entry_points = [CommandHandler('start', self.locationHandler)],
                 states = {
-                    self.LOCATION_BUTTON : [CallbackQueryHandler(self.languageButtonHandler)],
-                    self.LANGUAGE_BUTTON : [CallbackQueryHandler(self.sendMessageHandler)]
+                    self.LOCATION_BUTTON : [CallbackQueryHandler(self.languageHandler)],
+                    self.LANGUAGE_BUTTON : [CallbackQueryHandler(self.messageHandler)]
                 },
                 fallbacks = [CommandHandler('cancel',self.fallbackHandler)],
                 map_to_parent={
@@ -50,11 +50,16 @@ class CornerstoneChatbot:
     # 일반 함수
     # 챗봇 소개 및 DB 연동
     # 챗봇 명령어 등을 설명하기 위함
-    # self.locationButtonHandler callback 함수에서 호출 됨, callback 함수 이외에 제일 먼저 호출 되는 함수
+    # self.locationHandler에서 호출 됨, callback 함수 이외에 제일 먼저 호출 되는 함수
     '''
     def introduction(self, update:Update):
             update.message.reply_text('안녕하세요, 코너스톤 챗봇입니다.')
-        
+
+    '''
+    # 일반 함수
+    # 챗봇의 사용법을 알려주기 위함
+    # self.locationHandler에서 호출 됨
+    '''    
     def showHint(self, update:Update):
             table = pt.PrettyTable(['function', 'enter'])
             table.align['function'] = 'm'
@@ -72,10 +77,10 @@ class CornerstoneChatbot:
     '''
     # 일반 함수
     # DB에 데이터를 저장하기 위한 함수
-    # self.sendMessageHandler에서 호출
+    # self.messageHandler에서 호출
     # 반환 값은 사용되지 않음
     '''
-    def dbHandler(self) -> None:
+    def dbControl(self):
         # User ID : self.user_id
         # Location : self.location
         # Language : self.language
@@ -85,7 +90,7 @@ class CornerstoneChatbot:
     # 일반 함수
     # 버튼 메뉴를 생성하기 위한 함수
     # 복붙 코드라 정확한 기능을 이해하지 못함
-    # self.locationButtonHandler와 self.languageButtonHandler에서
+    # self.locationHandlerr와 self.languageHandler에서
         InlineKeyboardMarkup 함수의 인자로 사용 됨
     '''
     def build_menu(self, buttons, n_cols, header_buttons=None, footer_buttons=None):
@@ -97,6 +102,19 @@ class CornerstoneChatbot:
         return menu
 
     '''
+    # 일반 함수
+    # 전달된 btnText_list를 이용해 버튼 생성
+    '''
+    def createButton(self, btnText_list):
+        btn_list = []
+
+        for text in btnText_list:
+            btn_list.append(InlineKeyboardButton(text, callback_data=text))
+
+        show_markup = InlineKeyboardMarkup(self.build_menu(btn_list, len(btn_list)))
+        return show_markup
+
+    '''
     # callback 함수
     # ConversationHandler의 entry_point에 할당 된 Handler에서 호출 됨
     # 즉, 챗봇 시작 시, 가장 먼저 호출 되는 Handler
@@ -104,59 +122,58 @@ class CornerstoneChatbot:
     # update.effective_chat.id를 통해 현재 챗봇을 이용 중인 user의 ID를 가져옴
     # show_markup : btn_list를 기반으로 만들어진 버튼 메뉴가 담기는 변수
     '''
-    def locationButtonHandler(self, update:Update, context:CallbackContext) -> None:
-        self.introduction(update=update)
-        self.showHint(update=update)
+    def locationHandler(self, update:Update, context:CallbackContext):
         self.user_id = update.effective_chat.id
-        btn_list = []
-        btn_list.append(InlineKeyboardButton("대전광역시", callback_data="대전광역시"))
-        btn_list.append(InlineKeyboardButton("충북", callback_data="충북"))
+        self.introduction(update)
+        self.showHint(update)
+       
+        btnText_list = [
+            '대전광역시', '충북'
+        ]
 
-        show_markup = InlineKeyboardMarkup(self.build_menu(btn_list, len(btn_list)))
-        context.bot.send_message(chat_id=self.user_id, 
-            text='거주하는 지역을 선택해 주세요.', 
-            reply_markup=show_markup
+        context.bot.send_message(
+            chat_id=self.user_id, 
+            text = '거주하는 지역을 선택해 주세요.', 
+            reply_markup = self.createButton(btnText_list)
         )
         return self.LOCATION_BUTTON
 
     '''
     # callback 함수
-    # self.locationButtonHandler 이후 동작(state에서 Return const에 따라 결정 됨) 
-    # self.locationButtonHandler을 통해 생성된 버튼(지역)을 누르면 update.callback_query.data에 
+    # self.locationHandler 이후 동작(state에서 Return const에 따라 결정 됨) 
+    # self.locationHandler을 통해 생성된 버튼(지역)을 누르면 update.callback_query.data에 
         해당 버튼의 callback_data가 저장 됨
     # 반환 값을 통해 다음 CallbackQueryHandler를 결정함(ConversationHandler가 자동으로)
     # show_markup : btn_list를 기반으로 만들어진 버튼 메뉴가 담기는 변수
     '''
-    def languageButtonHandler(self, update:Update, context:CallbackContext) -> None:
+    def languageHandler(self, update:Update, context:CallbackContext):
         self.location = update.callback_query.data
         print(self.location)
 
-        btn_list = []
-        btn_list.append(InlineKeyboardButton("영어", callback_data="영어"))
-        btn_list.append(InlineKeyboardButton("일본어", callback_data="일본어"))
-        btn_list.append(InlineKeyboardButton("중국어", callback_data="중국어"))
+        btnText_list = [
+            '영어', '일본어', '중국어'
+        ]
 
-        show_markup = InlineKeyboardMarkup(self.build_menu(btn_list, len(btn_list)))
-        context.bot.send_message(chat_id=self.user_id, 
-            text='사용하실 언어를 선택해 주세요.', 
-            reply_markup=show_markup
-            )
-
+        context.bot.send_message(
+            chat_id=self.user_id, 
+            text = '사용하실 언어를 선택해 주세요.', 
+            reply_markup = self.createButton(btnText_list)
+        )
         return self.LANGUAGE_BUTTON
 
     '''
     # callback 함수
-    # self.languageButtonHandler 이후 동작(state에서 Return const에 따라 결정 됨) 
-    # self.locationButtonHandler을 통해 생성된 버튼(지역)을 누르면 update.callback_query.data에 
+    # self.languageHandler 이후 동작(state에서 Return const에 따라 결정 됨) 
+    # self.locationHandler을 통해 생성된 버튼(지역)을 누르면 update.callback_query.data에 
         해당 버튼의 callback_data가 저장 됨
     # 이 Handler 호출 시점에 self.user_id, self.location, self.language에 값이 들어 가게 됨
-    # 따라서, 이 Handler가 호출 되는 시점에, DB에 데이터를 저장하도록 설계함(self.dbHandler 함수 호출)
+    # 따라서, 이 Handler가 호출 되는 시점에, DB에 데이터를 저장하도록 설계함(self.dbControl 함수 호출)
     # message : DB에서 얻어 온 실제 재난 문자가 저장 되는 변수
     '''
-    def sendMessageHandler(self, update:Update, context:CallbackContext) -> None:
+    def messageHandler(self, update:Update, context:CallbackContext):
         self.language = update.callback_query.data
         print(self.language)
-        self.dbHandler()
+        self.dbControl()
 
         message = '긴급 재난 문자'
 
